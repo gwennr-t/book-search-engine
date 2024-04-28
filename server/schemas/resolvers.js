36409eecb1d367242512663dd2750ffa26b7270a
gwennr-t <gwennt321@gmail.com>
2024-04-28 16:36:00 -0500

    Updated schemas files based on user-controller.js. Not complete

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,9 +2,29 @@ const {User, Book} = require('../models');
 
 const resolvers = {
     Query: {
+        user: async () => {
+            return User.findById({});
+        },
+        books: async (parent, {_id}) => {
+            const params = _id ? {_id} : {};
+            return Book.find(params);
+        },
     },
-    Mutation: {
-    },
+//     Mutation: {
+//         createUser: async (parent, args) => {
+//             const user = await User.create(args);
+//             return user;
+//         },
+//         login: async (parent, args) => {
+//             return
+//         },
+//         saveBook: async (parent, args) => {
+//             return
+//         },
+//         deleteBook: async (parent, args) => {
+//             return
+//         },
+//     },
 };
 
-module.exprots = resolvers;
+module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,28 +1,19 @@
 const typeDefs = `
     type User {
-        // _id
-
-        // username
-
-        // email
-
-        // bookCount
-
-        // savedBooks (This will be an array of the Book type.)
+        _id: ID!
+        name: String!
+        email: String!
+        bookCount: Int
+        savedBooks: []
     }
 
     type Book {
-        // bookId (Not the _id, but the book's id value returned from Google's Book API.)
-
-        // authors (An array of strings, as there may be more than one author.)
-        
-        // description
-        
-        // title
-        
-        // image
-        
-        // link
+        bookId: ID!
+        authors: []
+        description: String!
+        title: String!
+        image: String!
+        link: String!
     }
 
     type Auth {
@@ -37,13 +28,13 @@ const typeDefs = `
     }
 
     type Mutation {
-        // login: Accepts an email and password as parameters; returns an Auth type.
+        // createUser: Accepts a username, email, and password as parameters; returns an Auth type.
 
-        // addUser: Accepts a username, email, and password as parameters; returns an Auth type.
+        // login: Accepts an email and password as parameters; returns an Auth type.
         
         // saveBook: Accepts a book author's array, description, title, bookId, image, and link as parameters; returns a User type. (Look into creating what's known as an input type to handle all of these parameters!)
         
-        // removeBook: Accepts a book's bookId as a parameter; returns a User type.
+        // deleteBook: Accepts a book's bookId as a parameter; returns a User type.
 
     }
 `;
