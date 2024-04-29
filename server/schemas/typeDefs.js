d0a3b179e05c3d3376100cb5869c68695fec0cac
gwennr-t <gwennt321@gmail.com>
2024-04-28 20:04:05 -0500

    Updated typeDefs to fix errors

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,20 +2,20 @@ const {gql} = require('apollo-server-express');
 
 const typeDefs = gql `
     type User {
-        _id: ID!
-        name: String!
-        email: String!
+        _id: ID
+        name: String
+        email: String
         bookCount: Int
-        savedBooks: []
+        savedBooks: [Book]!
     }
 
     type Book {
         bookId: ID
-        authors: []
-        description: String!
-        title: String!
-        image: String!
-        link: String!
+        authors: [Author]!
+        description: String
+        title: String
+        image: String
+        link: String
     }
 
     type Auth {
@@ -25,18 +25,18 @@ const typeDefs = gql `
 
     type Query {
         me: User
-
     }
 
     type Mutation {
-        createUser: (username: String!, email: String!, password: String!) :Auth
+        createUser(username: String!, email: String!, password: String!): Auth
 
-        login: (email: String!, password: String!) :Auth
+        login(email: String!, password: String!): Auth
         
         // (Look into creating what's known as an input type to handle all of these parameters!)//
-        saveBook: (savedBooks: [], description: String!, title: String!, bookId: String!, image: String!, link: String!) :User
+
+        saveBook(savedBooks: [Book], description: String!, title: String!, bookId: String!, image: String!, link: String!): User
         
-        deleteBook: (bookId: String!) :User
+        deleteBook(userId: ID!, commentId: ID!): User
     }
 `;
 
